afadbbd782e9fa2545b5609520bf27bfe351a532
Jordan <jkimys2@gmail.com>
2023-12-21 18:04:07 -0500

    file update

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,6 @@ var searchBtn = $("#search-btn");
 var searchCard = $(".search-card");
 var resultsCard = $(".results-card");
 var currentSearchCard = $("#current-search");
-var fiveDayCard = $("#five-day-card");
 
 // function for search button
 function handleSearch(event) {
@@ -27,17 +26,18 @@ function handleSearch(event) {
     });
   };
 
-
   var displayCurrentWeather = function (data, cityName) {
     var currentCity = document.querySelector("#current-city");
     var currentWeather = $("#current-weather");
     currentWeather.empty();
+    console.log(data)
     currentCity.textContent = cityName + ", " + data.name;
     var icon = document.createElement("img");
     icon.setAttribute(
       "src",
       "http://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png"
     );
+    console.log(data);
     currentCity.append(icon);
     var tempEl = $("<h3>")
       .addClass("card-text")
@@ -50,8 +50,8 @@ function handleSearch(event) {
       .text("Humidity: " + data.main.humidity);
     currentWeather.append(tempEl, windEl, humidityEl);
     getForecast(data.coord.lat, data.coord.lon);
+    saveSearch(cityName);
   };
-
 
   var getLatLon = function () {
     var cityInputVal = document.querySelector("#city-input").value;
@@ -72,7 +72,6 @@ function handleSearch(event) {
   };
   getLatLon();
 
-
   var getForecast = function (lat, lon) {
     var getWeatherUrl =
       "https://api.openweathermap.org/data/2.5/forecast?lat=" +
@@ -89,6 +88,7 @@ function handleSearch(event) {
             var forecastTime = data.list[i].dt_txt.split(" ").pop();
             if (forecastTime === "12:00:00") {
               forecastArray.push(data.list[i]);
+              displayForecast(data.list[i]);
             }
           }
           console.log(forecastArray);
@@ -97,9 +97,16 @@ function handleSearch(event) {
     });
   };
 
-  var displayForecast = function() {
+  var displayForecast = function () {
+    var fiveDayCard = $("#five-day-card");
+    var fiveDayText = $("#five-day-text");
+    fiveDayText.text("5 Day Forecast");
+  };
+  var saveSearch = function (city) {
+    console.log(city);
+    localStorage.setItem("City", city)
 
-  }
+  };
 }
 // click event for search button
 searchBtn.click(handleSearch);
